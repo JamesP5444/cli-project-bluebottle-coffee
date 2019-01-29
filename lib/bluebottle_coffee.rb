e30b4de66314fc43b8475ba6614b69e6d04358e0
yukijina <yukijina@gmail.com>
2019-01-28 19:43:43 -0800

    added relative path

--- lib/bluebottle_coffee.rb
+++ lib/bluebottle_coffee.rb
@@ -1,4 +1,11 @@
-require "bluebottle_coffee/version"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative "./bluebottle_coffee/version"
+require_relative "./bluebottle_coffee/cli"
+require_relative "./bluebottle_coffee/scraper"
+require_relative "./bluebottle_coffee/bluebottle"
 
 module BluebottleCoffee
   class Error < StandardError; end
